f27baf6a81485b4b919d2146fc2a23a89badc515
sharmin <sharminh@163.com>
2019-12-27 17:54:50 +0800

    redux style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { GlobalStyle } from  './style';
+import React from 'react'
+import { GlobalStyle } from  './style'
 import { IconStyle } from './assets/iconfont/iconfont'
+import routes from './routes/index'
+import { HashRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+import { Provider } from 'react-redux'
+import store from './store/index'
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <div className="App">
+          <GlobalStyle></GlobalStyle>
+          <IconStyle></IconStyle>
+          { renderRoutes(routes) }
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
