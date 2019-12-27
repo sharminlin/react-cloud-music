5c04eb3b5a84773ff86baf52a2cf1e6e22db6620
sharmin <sharminh@163.com>
2019-12-27 17:03:10 +0800

    iconfount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { GlobalStyle } from  './style';
+import { IconStyle } from './assets/iconfont/iconfont'
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
