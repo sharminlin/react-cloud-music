26c2ea81b07f250fae11cdc5ae8bc3e8457c6d66
sharmin <sharminh@163.com>
2020-08-05 14:39:15 +0800

    base url

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const baseUrl = 'http://localhost:3000/snetease'
+// export const baseUrl = 'http://localhost:3000/snetease'
+export const baseUrl = 'http://sharmin.top/snetease'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl
